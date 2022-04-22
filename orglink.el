6b2ac90af9f36e99aa2d4f56ffb237a80341beb8
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:26:14 +0200

    Refresh library header

--- orglink.el
+++ orglink.el
@@ -1,30 +1,32 @@
-;;; orglink.el --- use Org Mode links in other modes  -*- lexical-binding: t -*-
+;;; orglink.el --- Use Org Mode links in other modes  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2004-2022  Free Software Foundation, Inc.
-;; Copyright (C) 2013-2022  Jonas Bernoulli
+;; Copyright (C) 2004-2022 Free Software Foundation, Inc.
+;; Copyright (C) 2013-2022 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/orglink
-;; Keywords: hypertext
+;; Keywords: hypermedia
 
-;; Package-Requires: ((emacs "25.1") (compat "28.1.1.0") (org "9.5") (seq "2.23"))
+;; Package-Requires: (
+;;     (emacs "25.1")
+;;     (compat "28.1.1.0")
+;;     (org "9.5")
+;;     (seq "2.23"))
+
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is not part of Org Mode.
-;; This file is not part of GNU Emacs.
-
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
