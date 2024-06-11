3b30ab39c6ac6ffa7328c81ea5fa8ead723a1979
Jonas Bernoulli <jonas@bernoul.li>
2024-06-11 22:30:54 +0200

    Remove obsolete backward compatibility kludge
    `font-lock-flush' and `font-lock-ensure' were added ten
    years ago in Emacs 25.1.  We already depend on Emacs 25.1.

--- orglink.el
+++ orglink.el
@@ -147,14 +147,10 @@ On the links the following commands are available:
          (kill-local-variable 'font-lock-unfontify-region-function)
          (kill-local-variable 'org-mouse-map)))
   (when font-lock-mode
-    (if (and (fboundp 'font-lock-flush)
-             (fboundp 'font-lock-ensure))
-        (save-restriction
-          (widen)
-          (font-lock-flush)
-          (font-lock-ensure))
-      (with-no-warnings
-        (font-lock-fontify-buffer)))))
+    (save-restriction
+      (widen)
+      (font-lock-flush)
+      (font-lock-ensure))))
 
 ;;;###autoload
 (define-globalized-minor-mode global-orglink-mode
